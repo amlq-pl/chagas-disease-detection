3ec104e7d49648db316459f540966155d69cd753
Szymon Gaczoł <szymon.gaczol@swmansion.com>
2026-06-12 01:59:06 +0200

    update merge_and_process_data dostosowane do mergowania samitrop z code15

--- merge_and_process_data.py
+++ merge_and_process_data.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-Łączy i przetwarza macierze z prepare_ptbxl_data.py (label 0) i prepare_samitrop_data.py (label 1).
+Łączy i przetwarza macierze z prepare_samitrop_data.py i prepare_code15_data.py
 
 Skrypty prepare_* zapisują surowe okna (bez filtra i normalizacji).
 Tutaj robimy wspólne przetwarzanie obu zbiorów naraz:
 1. filtr pasmowy low..high Hz - górny próg sterowany argumentem --high,
 2. standaryzacja z-score
-3. sklejenie PTB-XL + SaMi-Trop w jedną macierz na każdą częstotliwość
+3. sklejenie SaMi-Trop + CODE-15% w jedną macierz na każdą częstotliwość
 
+Obie bazy są natywnie 400 Hz, a 100/250 Hz mamy z resamplingu po obu stronach.
 Łączymy (obie strony mają zgodne kształty, 7 s okna):
-- 100 Hz natywne   -> ecg_merged_100hz_native.npy    : ecg_ptbxl_100hz (natywne)   + ecg_samitrop_100hz_resampled
-- 100 Hz resampled -> ecg_merged_100hz_resampled.npy : ecg_ptbxl_100hz_resampled    + ecg_samitrop_100hz_resampled
-- 250 Hz           -> ecg_merged_250hz.npy           : ecg_ptbxl_250hz              + ecg_samitrop_250hz_resampled
-- 400 Hz           -> ecg_merged_400hz.npy           : ecg_ptbxl_400hz              + ecg_samitrop_400hz
+- 100 Hz -> ecg_merged_100hz_resampled.npy : ecg_samitrop_100hz_resampled + ecg_code15_100hz_resampled
+- 250 Hz -> ecg_merged_250hz.npy           : ecg_samitrop_250hz_resampled + ecg_code15_250hz_resampled
+- 400 Hz -> ecg_merged_400hz.npy           : ecg_samitrop_400hz           + ecg_code15_400hz
 
 Użycie:
     python3 merge_and_process_data.py [-i semi-processed-data] [-o processed-data] [--low 0.5] [--high 40]
@@ -28,18 +28,17 @@ import sys
 import numpy as np
 from scipy.signal import butter, filtfilt
 
-# Częstotliwość -> (fs, plik PTB-XL, plik Sami-Trop). Macierze mają układ (N, 12, L)
+# Częstotliwość -> (fs, plik SaMi-Trop, plik CODE-15%, plik wyjściowy). Macierze mają układ (N, 12, L)
 SPECS = [
-    (100, 'ecg_ptbxl_100hz.npy',           'ecg_samitrop_100hz_resampled.npy', 'ecg_merged_100hz_native.npy'),     # natywne PTB-XL + resampled SaMi-Trop
-    (100, 'ecg_ptbxl_100hz_resampled.npy', 'ecg_samitrop_100hz_resampled.npy', 'ecg_merged_100hz_resampled.npy'),  # oba resampled
-    (250, 'ecg_ptbxl_250hz.npy',           'ecg_samitrop_250hz_resampled.npy', 'ecg_merged_250hz.npy'),
-    (400, 'ecg_ptbxl_400hz.npy',           'ecg_samitrop_400hz.npy',           'ecg_merged_400hz.npy'),
+    (100, 'ecg_samitrop_100hz_resampled.npy', 'ecg_code15_100hz_resampled.npy', 'ecg_merged_100hz_resampled.npy'),
+    (250, 'ecg_samitrop_250hz_resampled.npy', 'ecg_code15_250hz_resampled.npy', 'ecg_merged_250hz_resampled.npy'),
+    (400, 'ecg_samitrop_400hz.npy',           'ecg_code15_400hz.npy',           'ecg_merged_400hz.npy'),
 ]
 
 
 def get_parser():
     parser = argparse.ArgumentParser(
-        description='Łączy i przetwarza (filtr + standaryzacja) macierze PTB-XL i SaMi-Trop.')
+        description='Łączy i przetwarza (filtr + standaryzacja) macierze SaMi-Trop i CODE-15%.')
     parser.add_argument('-i', '--input_folder', type=str, default='semi-processed-data',
                         help='Folder z plikami .npy z prepare_* (domyślnie semi-processed-data).')
     parser.add_argument('-o', '--output_folder', type=str, default='processed-data',
@@ -79,36 +78,38 @@ def run(args):
     os.makedirs(out, exist_ok=True)
 
     # Etykiety, metadane, id - łączone raz
-    labels_p = load(inp, 'labels_ptbxl.npy')
     labels_s = load(inp, 'labels_samitrop.npy')
-    n_p, n_s = len(labels_p), len(labels_s)
+    labels_c = load(inp, 'labels_code15.npy')
+    n_s, n_c = len(labels_s), len(labels_c)
 
-    labels = np.concatenate([labels_p, labels_s])
-    metadata = np.concatenate([load(inp, 'metadata_ptbxl.npy'), load(inp, 'metadata_samitrop.npy')])
-    # Globalnie unikalne id z prefiksem zbioru ('ptbxl_<id>' / 'samitrop_<id>'), bo numery z obu baz mogą się powtarzać
-    ids_p = load(inp, 'ecg_ids_ptbxl.npy')
+    labels = np.concatenate([labels_s, labels_c])
+    metadata = np.concatenate([load(inp, 'metadata_samitrop.npy'), load(inp, 'metadata_code15.npy')])
+    # Globalnie unikalne id z prefiksem zbioru ('samitrop_<id>' / 'code15_<id>'), bo numery z obu baz mogą się powtarzać
     ids_s = load(inp, 'exam_ids_samitrop.npy')
-    ids = np.array([f'ptbxl_{i}' for i in ids_p] + [f'samitrop_{i}' for i in ids_s])
-    source = np.concatenate([np.zeros(n_p, dtype=np.int64), np.ones(n_s, dtype=np.int64)])  # 0=PTB-XL, 1=SaMi-Trop
+    ids_c = load(inp, 'exam_ids_code15.npy')
+    ids = np.array([f'samitrop_{i}' for i in ids_s] + [f'code15_{i}' for i in ids_c])
+    source = np.concatenate([np.zeros(n_s, dtype=np.int64), np.ones(n_c, dtype=np.int64)])  # 0=SaMi-Trop, 1=CODE-15%
 
     np.save(os.path.join(out, 'labels_merged.npy'), labels)
     np.save(os.path.join(out, 'metadata_merged.npy'), metadata)
     np.save(os.path.join(out, 'ids_merged.npy'), ids)
     np.save(os.path.join(out, 'source_merged.npy'), source)
 
-    print(f'PTB-XL: {n_p} recordów (label 0) | SaMi-Trop: {n_s} recordów (label 1) | razem: {n_p + n_s}')
+    n_pos = int((labels == 1).sum())
+    print(f'SaMi-Trop: {n_s} recordów | CODE-15%: {n_c} recordów | razem: {n_s + n_c}')
+    print(f'Pozytywnych w sumie: {n_pos} | negatywnych: {n_s + n_c - n_pos}')
     print(f'Filtr pasmowy: {args.low}-{args.high} Hz\n')
 
     # Dla każdego wpisu: wczytaj obie macierze, przefiltruj, standaryzuj, sklej i zapisz
-    for fs, ptbxl_name, samitrop_name, out_name in SPECS:
-        Xp = load(inp, ptbxl_name)
+    for fs, samitrop_name, code15_name, out_name in SPECS:
         Xs = load(inp, samitrop_name)
-        if Xp.shape[0] != n_p or Xs.shape[0] != n_s:
+        Xc = load(inp, code15_name)
+        if Xs.shape[0] != n_s or Xc.shape[0] != n_c:
             sys.exit(f'Niezgodna liczba recordów dla {out_name} (EKG vs etykiety).')
 
-        Xp = standardize(bandpass(Xp, fs, args.low, args.high))
         Xs = standardize(bandpass(Xs, fs, args.low, args.high))
-        X = np.concatenate([Xp, Xs], axis=0)
+        Xc = standardize(bandpass(Xc, fs, args.low, args.high))
+        X = np.concatenate([Xs, Xc], axis=0)
 
         np.save(os.path.join(out, out_name), X)
         print(f'  {out_name:32s}: {X.shape}')
